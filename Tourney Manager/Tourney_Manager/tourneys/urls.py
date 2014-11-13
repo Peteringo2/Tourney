4c6a8c0ef3df4feb2024e3e0ca4aa2edcd13a78c
CuRy <curyman@gmail.com>
2014-11-13 08:49:03 -0600

    tourneyhome

--- Tourney Manager/Tourney_Manager/tourneys/urls.py
+++ Tourney Manager/Tourney_Manager/tourneys/urls.py
@@ -4,5 +4,6 @@ from tourneys import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-    url(r'^profile/$' , views.profile, name='profile')
+    url(r'^profile/$' , views.profile, name='profile'),
+    url(r'^tourneyhome/$' , views.tourneyhome, name='tourneyhome')
 )
--- Tourney Manager/Tourney_Manager/tourneys/views.py
+++ Tourney Manager/Tourney_Manager/tourneys/views.py
@@ -2,9 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 
+from tourneys.models import Tourney
+
 # Create your views here.
 def index(request):
 	return render(request, 'tourneys/home.html', request)
 
 def profile(request):
 	return render(request, 'tourneys/profile.html', request)
+
+def tourneyhome(request):
+	return render(request, 'tourneys/tourneyhome.html', request)
